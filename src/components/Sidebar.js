46abd2b1d99270070d8cf4f184536293ae212031
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-24 11:57:51 +0100

    all needed components added to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
+// Import data
+import { sidebarItems } from "../data/SidebarData";
+
 // Imports of icons
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import AddIcon from "@material-ui/icons/Add";
 
 // Imports for styling
 import styled from "styled-components";
@@ -13,6 +17,24 @@ const Sidebar = () => {
           <AddCircleOutlineIcon />
         </NewMessage>
       </WorkSpaceContainer>
+      <MainChannels>
+        {sidebarItems.map((item) => (
+          <MainChannelItem>
+            {item.icon}
+            {item.text}
+          </MainChannelItem>
+        ))}
+      </MainChannels>
+      <ChannelsContainer>
+        <NewChannelContainer>
+          <div>Channels</div>
+          <AddIcon />
+        </NewChannelContainer>
+        <ChannelsList>
+          <Channel># Channel 1</Channel>
+          <Channel># Channel 2</Channel>
+        </ChannelsList>
+      </ChannelsContainer>
     </Container>
   );
 };
@@ -45,6 +67,48 @@ const NewMessage = styled.div`
   color: #3f0e40;
   fill: #3f0e40;
   border-radius: 50%50%;
+`;
+
+const MainChannels = styled.div``;
+
+const MainChannelItem = styled.div`
+  height: 28px;
+  display: grid;
+  grid-template-columns: 15% auto;
+  align-items: center;
+  padding: 0 0 0 19px;
+  cursor: pointer;
+  color: rgb(188, 171, 188);
+  :hover {
+    background: #350d36;
+  }
+`;
+
+const ChannelsContainer = styled.div`
+  margin: 10px 0 0 0;
+  color: rgb(188, 171, 188);
+`;
+
+const NewChannelContainer = styled.div`
+  height: 28px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 12px 0 19px;
+`;
+
+const ChannelsList = styled.div``;
+
+const Channel = styled.div`
+  height: 28px;
+  display: flex;
+  align-items: center;
+  padding: 0 0 0 19px;
+  cursor: pointer;
+
+  :hover {
+    background: #350d36;
+  }
 `;
 
 export default Sidebar;
