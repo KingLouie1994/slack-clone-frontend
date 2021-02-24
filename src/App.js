fcb688616d99f7289f20664e955532065a773425
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2021-02-24 09:59:11 +0100

    created all basic headers of the ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 // Imports from react router dom
 import { Route, Switch } from "react-router-dom";
 
-// Import of global style
-import GlobalStyle from "./Styles/GlobalStyle";
+// Imports of components
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
 
-function App() {
+// Imports for styling
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
+
+const App = () => {
   return (
     <div>
       <GlobalStyle />
-      <Switch>
-        <Route path="/" exact>
-          Login Page
-        </Route>
-        <Route path="/room" exact>
-          Chat Page
-        </Route>
-      </Switch>
+      <Container>
+        <Header />
+        <Main>
+          <Sidebar />
+          <Switch>
+            <Route path="/" exact>
+              <Login />
+            </Route>
+            <Route path="/room" exact>
+              <Chat />
+            </Route>
+          </Switch>
+        </Main>
+      </Container>
     </div>
   );
-}
+};
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 38px auto;
+`;
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: 260px auto;
+  background-color: blue;
+`;
 
 export default App;
